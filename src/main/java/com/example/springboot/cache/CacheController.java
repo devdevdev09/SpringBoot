913094c7b97cc65e2f6213926cedc08102c8ddea
dev09 <devdevdevdevdev09@gmail.com>
2020-08-04 14:53:07 +0900

    add spring boot cache example

--- src/main/java/com/example/springboot/cache/CacheController.java
+++ src/main/java/com/example/springboot/cache/CacheController.java
@@ -1,21 +1,33 @@
 package com.example.springboot.cache;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableCaching
+@RestController
 public class CacheController {
 
-    @Cacheable(value = "test")
-    public void testCache() {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    int TEST_COUNT = 0;
+
+    @Cacheable(value = "addCount")
+    @RequestMapping(value="/add")
+    public int testCache(@RequestParam int num) {
+        logger.info("=========start=========");
+        TEST_COUNT++;
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        logger.info("=========end=========");
 
-        System.out.println("test cache~~~~");
-
+        return TEST_COUNT;
     }
 }
--- src/main/java/com/example/springboot/cache/CacheMainApplication.java
+++ src/main/java/com/example/springboot/cache/CacheMainApplication.java
@@ -1,15 +1,13 @@
 package com.example.springboot.cache;
 
-import com.example.springboot.DemoApplication;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-public class CacheMain {
+public class CacheMainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CacheMain.class, args);
+        SpringApplication.run(CacheMainApplication.class, args);
     }
 }
