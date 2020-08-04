6f267dfa2acc2d91a4175506e3b61a69ec2f4a98
dev09 <devdevdevdevdev09@gmail.com>
2020-08-04 17:04:35 +0900

    update fileupload exam

---
+++ src/main/java/com/example/springboot/fileupload/FileUploadService.java
@@ -0,0 +1,10 @@
+package com.example.springboot.fileupload;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileUploadService {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+}
