c75084dec6407468a3c24f825b6286386af73a0b
dev09 <devdevdevdevdev09@gmail.com>
2020-08-04 17:51:14 +0900

    파일업로드 개발중...

--- src/main/java/com/example/springboot/fileupload/FileUploadController.java
+++ src/main/java/com/example/springboot/fileupload/FileUploadController.java
@@ -1,20 +1,44 @@
 package com.example.springboot.fileupload;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@RequestMapping(value= "/fileupload")
+@RequestMapping(value = "/fileupload")
 @Controller
 public class FileUploadController {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@GetMapping("/index")
-	public String fileUploadIndex(Model model){
+	public String fileUploadIndex(Model model) {
 		return "fileupload/index";
+	}
+
+	@PostMapping("/upload")
+	public String fileUPload(@RequestParam("file") MultipartFile file) {
+		logger.info("fileUpload :: ");
+		logger.info(file.getContentType());
+		try {
+			File fi = new File(file.getOriginalFilename());
+			file.transferTo(fi);
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "fileupload/fileinfo";
 	}
 }
